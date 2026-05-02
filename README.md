ae152756fa990f551cd8e74e4ec4be0ef5117ce3
Arthur Zheng <arthurzheng150@gmail.com>
2026-05-02 13:18:25 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# DS18B20 Sensor
+
+![Version](https://img.shields.io/github/v/tag/openblock-plugin/ds18b20?label=version)
+![License](https://img.shields.io/badge/license-MIT-green.svg)
+![Type](https://img.shields.io/badge/type-extension-orange.svg)
+![Tags](https://img.shields.io/badge/tags-sensor-lightgrey.svg)
+
+Single bus digital temperature sensor based on DS18B20.
+
+## Documentation
+
+For detailed usage instructions, please visit:
+
+**[OpenBlock Wiki](https://wiki.openblock.cc)**
---
+++ src/translations.js
@@ -0,0 +1,71 @@
+/* eslint-disable quote-props */
+/* eslint-disable max-len */
+/**
+ * Translation file for this resouce.
+ *
+ * IMPORTANT:
+ * - The "en" (English) section is automatically generated from source files.
+ *   Do NOT modify the "en" section manually.
+ * - Other language sections (e.g., "zh-cn", "zh-tw", "ja", etc.) should be
+ *   manually translated by you.
+ * - When you run the extraction script again, only the "en" section will be
+ *   updated. Your manual translations in other languages will be preserved.
+ *
+ * Structure:
+ * - interface: translations for name/description (used by GUI formatMessage)
+ * - extensions: translations for extension blocks (used by VM formatMessage)
+ * - blocks: translations for Blockly blocks (used by Blockly.Msg)
+ */
+
+export default {
+    'interface': {
+        'en': {
+            'ds18b20.name': 'DS18B20 Sensor',
+            'ds18b20.description': 'Single bus digital temperature sensor based on DS18B20.'
+        },
+        'ru': {
+            'ds18b20.name': 'DS18B20 Sensor',
+            'ds18b20.description': 'Single bus digital temperature sensor based on DS18B20.'
+        },
+        'zh-cn': {
+            'ds18b20.name': 'DS18B20 Sensor',
+            'ds18b20.description': 'Single bus digital temperature sensor based on DS18B20.'
+        },
+        'zh-tw': {
+            'ds18b20.name': 'DS18B20 Sensor',
+            'ds18b20.description': 'Single bus digital temperature sensor based on DS18B20.'
+        }
+    },
+    'extensions': {
+        'en': {},
+        'ru': {},
+        'zh-cn': {},
+        'zh-tw': {}
+    },
+    'blocks': {
+        'en': {
+            'DS18B20_CATEGORY': 'DS18B20',
+            'DS18B20_INIT': 'init ds18b20 pin %1 quantity %2',
+            'DS18B20_READALLTEMPERATURES': 'ds18b20 read all temperatures',
+            'DS18B20_GETTEMPERATURE': 'ds18b20 %1 temperatures %2'
+        },
+        'ru': {
+            'DS18B20_CATEGORY': 'DS18B20',
+            'DS18B20_INIT': 'init ds18b20 pin %1 quantity %2',
+            'DS18B20_READALLTEMPERATURES': 'ds18b20 read all temperatures',
+            'DS18B20_GETTEMPERATURE': 'ds18b20 %1 temperatures %2'
+        },
+        'zh-cn': {
+            'DS18B20_CATEGORY': 'DS18B20',
+            'DS18B20_INIT': 'init ds18b20 pin %1 quantity %2',
+            'DS18B20_READALLTEMPERATURES': 'ds18b20 read all temperatures',
+            'DS18B20_GETTEMPERATURE': 'ds18b20 %1 temperatures %2'
+        },
+        'zh-tw': {
+            'DS18B20_CATEGORY': 'DS18B20',
+            'DS18B20_INIT': 'init ds18b20 pin %1 quantity %2',
+            'DS18B20_READALLTEMPERATURES': 'ds18b20 read all temperatures',
+            'DS18B20_GETTEMPERATURE': 'ds18b20 %1 temperatures %2'
+        }
+    }
+};
